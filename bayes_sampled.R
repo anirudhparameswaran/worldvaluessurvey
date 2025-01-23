6cb38b035f3c8db580f0e7df407668ca233ee065
Anirudh Parameswaran <anirudhparameswaran@gmail.com>
2025-01-23 14:22:25 +0100

    added plots

--- bayes_sampled.R
+++ bayes_sampled.R
@@ -34,35 +34,56 @@ saveRDS(model1, file = "/Users/anirudhparameswaran/Desktop/worldvaluessurvey-mai
 formula2 <- voter_2 ~ age + sex + immigrant + income_level + education + god_importance + praying_frequency + ethics_score + satisfaction + (1 + sex + age + education + income_level | country)
 
 prior1 <- c(
-  # Older people tend to vote more than younger people
-  prior(normal(5, 10), class = "b", coef = "age"),
+  prior(normal(1, 3), class = "b", coef = "age"),
+  prior(normal(1, 3), class = "b", coef = "sexMale"),
+  prior(normal(-1, 3), class = "b", coef = "income_level"),
+  prior(normal(1, 3), class = "b", coef = "education"),
   
-  # Proportion of men voting is higher than women
-  prior(normal(5, 10), class = "b", coef = "sexMale"),
-  
-  # Poor people tend to vote more often than rich people
-  prior(normal(-5, 10), class = "b", coef = "income_level"),
-  
-  # Educated people tend to vote more often
-  prior(normal(5, 10), class = "b", coef = "education"),
-  
-  # there is high variance in data between USA and IND in terms of the relationship of sex, income_level and education
-  prior(cauchy(5, 10), class = "sd", group = "country", coef = "sexMale"),
-  prior(cauchy(5, 10), class = "sd", group = "country", coef = "education"),
-  prior(cauchy(5, 10), class = "sd", group = "country", coef = "income_level")
+  prior(student_t(3, 2, 3), class = "sd", group = "country", coef = "sexMale"),
+  prior(student_t(3, 2, 3), class = "sd", group = "country", coef = "education"),
+  prior(student_t(3, 2, 3), class = "sd", group = "country", coef = "income_level")
 )
 
 model2 <- brm(formula2,
               family = bernoulli(link = "logit"),
               data = sampled_data,
               iter = 1500,
-              chains = 2,
+              chains = 4,
               prior = prior1,
-              control = list(max_treedepth = 20, adapt_delta = 0.999),
+              control = list(max_treedepth = 17, adapt_delta = 0.999),
               cores = 4,
               seed = 42)
 
 summary(model2)
+
+conditional_effects(model2)
+
+transformed <- ggs(model1test)
+ggplot(filter(transformed, Parameter %in% colnames(as_draws_df(model2))),
+       aes(x   = Iteration,
+           y   = value, 
+           col = as.factor(Chain)))+
+  geom_line() +
+  geom_vline(xintercept = 1000)+
+  facet_grid(Parameter ~ . ,
+             scale  = 'free_y',
+             switch = 'y')+
+  labs(title = "Caterpillar Plots", 
+       col   = "Chains")
+
+ggplot(filter(transformed,
+              Parameter == "b_education", 
+              Iteration > 999),
+       aes(x = value)) +
+  geom_density(fill = "yellow", alpha = 0.5) +
+  geom_vline(xintercept = 0, col = "red", linewidth = 1) + 
+  scale_x_continuous(name = "Value", limits = c(-1, 3)) +
+  geom_vline(xintercept = summary(model1test)$fixed["education", "l-95% CI"],
+             col = "blue", linetype = 2) +
+  geom_vline(xintercept = summary(model1test)$fixed["education", "u-95% CI"],
+             col = "blue", linetype = 2) +
+  theme_light() +
+  labs(title = "Posterior Density of Education")
 
 saveRDS(model2, file = "/Users/anirudhparameswaran/Desktop/worldvaluessurvey-main/model2.rds")
 
